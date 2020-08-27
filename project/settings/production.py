06579298a179ac4c52da839bb8ff8a43f381f8bb
OmarFateh <OmarFateh@users.noreply.github.com>
2020-08-27 23:51:28 +0200

    postgres DB name

--- project/settings/production.py
+++ project/settings/production.py
@@ -93,7 +93,7 @@ else:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'elkarwanDB',
+        'NAME': 'elkarwandb',
         'USER': 'elkarwan',
         'PASSWORD': 'elkarwan1234',
         'HOST': 'localhost',
